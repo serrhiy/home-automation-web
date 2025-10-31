1e01f6bd3dde61b0e5068d77e831da77b4e600ca
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-10-31 13:13:21 +0200

    Finx operator.create endpoint

--- src/backend/main.js
+++ src/backend/main.js
@@ -7,10 +7,12 @@ const path = require('node:path');
 const repository = require('./repositories/PostgresDB.js')(db);
 const Router = require('./routers/FSRouter.js');
 const validator = require('./validator/SchemaValidator.js');
+const crypto = require('node:crypto');
 
 const webRestApiPath = path.join(__dirname, 'web_rest_api');
 
-const sandbox = { repository, db };
+const utils = { generateToken: crypto.randomUUID };
+const sandbox = { repository, utils };
 
 const main = async () => {
   const router = await new Router(webRestApiPath, loader, sandbox);
