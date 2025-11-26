467c7e982f04aacf645cd64f54a2a468a07a03b9
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-11-26 02:49:13 +0200

    Change database fields name

--- src/database/structure.sql
+++ src/database/structure.sql
@@ -1,70 +1,70 @@
-create table "billing_plans" (
+create table "billingPlans" (
   "id"    bigint generated always as identity,
   "name"  varchar(32) not null check (length("name") >= 3),
-  "price" numeric(10, 2)
+  "price" numeric(10, 2) not null
 );
 
-alter table "billing_plans" add constraint "pkBillingPlans" primary key ("id");
-create unique index "akBillingPlanNames" on "billing_plans" ("name");
+alter table "billingPlans" add constraint "pkBillingPlans" primary key ("id");
+create unique index "akBillingPlanNames" on "billingPlans" ("name");
 
-create table "billing_plan_info" (
-  "id"              bigint generated always as identity,
-  "billing_plan_id" bigint not null,
-  "created_at"      timestamptz default current_timestamp,
-  "ended_at"        timestamptz not null
+create table "billingPlanInfo" (
+  "id"            bigint generated always as identity,
+  "billingPlanId" bigint not null,
+  "createdAt"     timestamptz default current_timestamp,
+  "endedAt"       timestamptz not null
 );
 
-alter table "billing_plan_info" add constraint "pkBillingPlanInfo" primary key ("id");
-alter table "billing_plan_info" add constraint "fkBillingPlanInfoBillingPlanIdBillingPlansId"
-  foreign key ("billing_plan_id") references "billing_plans" ("id");
+alter table "billingPlanInfo" add constraint "pkBillingPlanInfo" primary key ("id");
+alter table "billingPlanInfo" add constraint "fkBillingPlanInfoBillingPlanIdBillingPlansId"
+  foreign key ("billingPlanId") references "billingPlans" ("id");
 
 create table "operators" (
-  "id"           bigint generated always as identity,
-  "label"        varchar(32) not null check (length("label") >= 3),
-  "plan_info_id" bigint not null,
-  "token"        varchar(36) NOT NULL,
-  "public_key"   text NOT NULL,
-  "created_at"   timestamptz default current_timestamp
+  "id"         bigint generated always as identity,
+  "label"      varchar(32) not null check (length("label") >= 3),
+  "planInfoId" bigint not null,
+  "token"      varchar(36) NOT NULL,
+  "publicKey"  text NOT NULL,
+  "createdAt"  timestamptz default current_timestamp
 );
 
 alter table "operators" add constraint "pkOperators" primary key ("id");
 alter table "operators" add constraint "fkOperatorsPlanInfoIdBillingPlanInfoId"
-  foreign key ("plan_info_id") references "billing_plan_info" ("id");
+  foreign key ("planInfoId") references "billingPlanInfo" ("id");
 create unique index "akOperatorsLabel" on "operators" ("label");
 create unique index "akOperatorsToken" on "operators" ("token");
 
 create table "panels" (
-  "id"               bigint generated always as identity,
-  "serial"           varchar(32) not null,
-  "operator_id"      bigint not null,
-  "software_version" varchar(32),
-  "status"           smallint not null
+  "id"              bigint generated always as identity,
+  "serial"          varchar(32) not null,
+  "operatorId"      bigint not null,
+  "softwareVersion" varchar(32),
+  "status"          smallint not null
 );
 
 alter table "panels" add constraint "pkPanels" primary key ("id");
 create unique index "akPanelsSerial" on "panels" ("serial");
 alter table "panels" add constraint "fkPanelsOperatorIdOperatorsId"
-  foreign key ("operator_id") references "operators" ("id");
+  foreign key ("operatorId") references "operators" ("id");
 
 create table "users" (
-  "id"         bigint generated always as identity,
-  "label"      varchar(32) not null check (length("label") >= 3),
-  "role"       smallint not null,
-  "panel_id"   bigint not null,
-  "created_at" timestamptz default current_timestamp
+  "id"        bigint generated always as identity,
+  "label"     varchar(32) not null check (length("label") >= 3),
+  "role"      smallint not null,
+  "panelId"   bigint not null,
+  "createdAt" timestamptz default current_timestamp
 );
 
 alter table "users" add constraint "pkUsers" primary key ("id");
 alter table "users" add constraint "fkUsersPanelIdPanelsId"
-  foreign key ("panel_id") references "panels" ("id");
+  foreign key ("panelId") references "panels" ("id");
 
 create table "devices" (
-  "id"        bigint generated always as identity,
-  "panel_id"  bigint not null,
-  "type"      smallint not null,
-  "subtype"   smallint not null
+  "id"      bigint generated always as identity,
+  "panelId" bigint not null,
+  "type"    smallint not null,
+  "subtype" smallint not null
 );
 
 alter table "devices" add constraint "pkDevices" primary key ("id");
 alter table "devices" add constraint "fkUDevicesPanelIdPanelsId"
-  foreign key ("panel_id") references "panels" ("id");
+  foreign key ("panelId") references "panels" ("id");
