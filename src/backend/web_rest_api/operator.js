e96fea6875b24d8c574e4d3f0ecb3e81c888f6a5
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-12-04 03:43:33 +0200

    Add cookie

--- src/backend/web_rest_api/operator.js
+++ src/backend/web_rest_api/operator.js
@@ -1,11 +1,12 @@
 'use strict';
 
 module.exports = ({ repository, utils }) => ({
-  create: async (publicKey, label) => {
+  create: async (publicKey, label, cookies) => {
     if (await repository.operators.exists(label)) {
       throw Error('Such operator already exists!');
     }
     const token = utils.generateToken();
+    cookies.set('token', token);
     return repository.operators.create(label, publicKey, token);
   },
 });
